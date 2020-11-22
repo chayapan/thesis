77468c2e8be2efcb63a41bf77673e0d7aaf3f82b
Chayapan Khannabha <chayapan@gmail.com>
2020-11-22 15:28:04 +0700

    Using imap for multiprocessing.

--- 4.0 Time-series Prediction/Error-Dist-ARIMA/SET50_arima_accuracy.py
+++ 4.0 Time-series Prediction/Error-Dist-ARIMA/SET50_arima_accuracy.py
@@ -1,10 +1,8 @@
 """ARIMA prediction accuracy estimate for SET50 stocks"""
-
-
 import multiprocessing as mp
 import itertools
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-
+import pandas as pd
 
 from SET50_arima_errors import get_series, collect_forecast_perf
 
@@ -22,50 +20,51 @@ def get_treatment_factors():
         treatments.append(tfactor)
     return treatments
 
+class ForecastMethod():
+    @staticmethod
+    def collect_accuracy_metric(work):
+        ticker = work[0] # unpack job
+        control_factor = work[1] 
+        print("Collect metric for series %s. Variation: %s." % (ticker, control_factor))
 
+        df = get_series(ticker)
+        # store report data
+        exprData = pd.DataFrame()
 
-def collect_accuracy_metric(work):
-    ticker = work[0] # unpack job
-    control_factor = work[1] 
-    print("Collect metric for series %s. Variation: %s." % (ticker, control_factor))
-    
-    df = get_series(ticker)
-    # store report data
-    exprData = pd.DataFrame()
-    
-    # unpack factors
-    morder = control_factor[0]
-    hist_window = control_factor[1]
-    forecast_days = control_factor[2]
-    data_transform = control_factor[3]
-    
-    print("%s :: ARIMA%s History: %s days Forecast: %s days" % (ticker, str(morder), hist_window, forecast_days))
-    start_time = time.time()
-    pred_res = collect_forecast_perf(ticker, df, ndays=forecast_days, window=hist_window, arima_order=morder)
-    dat = {
-        'series_id': ticker,
-        # 'forecast_setting': 'X_X_X_X_X_X',
-        'history_window': hist_window,
-        'forecast_days': forecast_days,
-        'arima_order': str(morder),
-        # Result from dataframe
-        'MAPE': pred_res['MAPE'][0],
-        'MSE': pred_res['MSE'][0],
-        'batches_tried': pred_res['batches_tried'][0],
-        'observations': pred_res['observations'][0],
-        'error_mean': pred_res[['Error (Pct)']].describe().T['mean'].values[0],
-        'error_std': pred_res[['Error (Pct)']].describe().T['std'].values[0],
-        'error_min': pred_res[['Error (Pct)']].describe().T['min'].values[0],
-        'error_max': pred_res[['Error (Pct)']].describe().T['max'].values[0]
-    }
-    # How log did it take to run this trial
-    elapse = time.time() - start_time
-    print("Treatment took %0.2f sec." % elapse)
-    dat['experiment_took_sec'] = elapse
-    # Add the descriptive stats for error percentage column
-    exprData = exprData.append(dat, ignore_index=True)
-    print("Save collected experimental data for %s." % ticker)
-    exprData.to_csv("output/arima_errors_%s.csv" % ticker, mode='a')
+        # unpack factors
+        morder = control_factor[0]
+        hist_window = control_factor[1]
+        forecast_days = control_factor[2]
+        data_transform = control_factor[3]
+
+        print("%s :: ARIMA%s History: %s days Forecast: %s days" % (ticker, str(morder), hist_window, forecast_days))
+        start_time = time.time()
+        pred_res = collect_forecast_perf(ticker, df, ndays=forecast_days, window=hist_window, arima_order=morder)
+        print("Finish experiment.")
+        dat = {
+            'series_id': ticker,
+            # 'forecast_setting': 'X_X_X_X_X_X',
+            'history_window': hist_window,
+            'forecast_days': forecast_days,
+            'arima_order': str(morder),
+            # Result from dataframe
+            'MAPE': pred_res['MAPE'][0],
+            'MSE': pred_res['MSE'][0],
+            'batches_tried': pred_res['batches_tried'][0],
+            'observations': pred_res['observations'][0],
+            'error_mean': pred_res[['Error (Pct)']].describe().T['mean'].values[0],
+            'error_std': pred_res[['Error (Pct)']].describe().T['std'].values[0],
+            'error_min': pred_res[['Error (Pct)']].describe().T['min'].values[0],
+            'error_max': pred_res[['Error (Pct)']].describe().T['max'].values[0]
+        }
+        # How log did it take to run this trial
+        elapse = time.time() - start_time
+        print("Treatment took %0.2f sec." % elapse)
+        dat['experiment_took_sec'] = elapse
+        # Add the descriptive stats for error percentage column
+        exprData = exprData.append(dat, ignore_index=True)
+        print("Save collected experimental data for %s." % ticker)
+        exprData.to_csv("output/arima_errors_%s.csv" % ticker, mode='a')
 
 if __name__ == '__main__':
     # collect_error_data('SCB')
@@ -81,12 +80,13 @@ if __name__ == '__main__':
     
     # Note about which multiprocessing to use:
     #. https://stackoverflow.com/questions/8533318/multiprocessing-pool-when-to-use-apply-apply-async-or-map
+    fmethod = ForecastMethod()
+    work = []
+    for job in itertools.product(SET50, control_factors):
+        work.append(job)
     with mp.Pool(16) as p:
-        work = []
-        for job in itertools.product(SET50, control_factors):
-            work.append(job)
         try:
-            p.map(collect_accuracy_metric, work)
+            p.imap(fmethod.collect_accuracy_metric, work, 4)
         except Exception as e:
             print("Job worker error %s" % str(e))
         p.close()
